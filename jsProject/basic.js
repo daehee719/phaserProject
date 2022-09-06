4531b3002386a1b39b7b58c506106fb0d91b4303
강대희 <daeheuigang@gmail.com>
2022-09-06 09:10:12 +0900

    플레이어 이동

--- jsProject/basic.js
+++ jsProject/basic.js
@@ -1,2 +1,73 @@
-    const canvas = document.querySelector("#gameCanvas");
-    const ctx = canvas.getContext("2d");
+const canvas = document.querySelector("#gameCanvas");
+const ctx = canvas.getContext('2d');
+let playerX = 10;
+let playerY = 10;
+let playerSize = 30;
+let speed = 150;//초당 200px
+let playerColor = "#f00";
+
+//이미 만들어진 객체로 쓸수 있음
+// 문서 전체에서 들음
+let keyArr=[];
+document.addEventListener("keydown",e=>
+{
+    keyArr[e.keyCode] = true;
+})
+document.addEventListener("keyup",e=>
+{
+    keyArr[e.keyCode] = false;
+})
+function Update()
+{
+    if(keyArr[37])
+    {
+        if(playerX==0)
+            playerX = 0;
+        else
+            playerX -= speed/60;
+    }
+    if(keyArr[38])
+    {
+        if(playerY==0)
+            playerY = 0;
+        else
+            playerY -= speed/60;
+    }
+    if(keyArr[39])
+    {
+        if(playerX == canvas.width - playerSize)
+            playerX == canvas.width - playerSize;
+        else
+            playerX += speed/60;
+    }
+    if(keyArr[40])
+    {
+        if(playerY == canvas.height - playerSize)
+            playerY == canvas.height - playerSize;
+        else
+            playerY += speed/60;
+    }
+
+    /*내일은 Player를 별도의 파일로 빼서 클래스화 시키는걸 할거임*/
+
+
+    
+}
+function Render()
+{
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = playerColor
+    ctx.fillRect(playerX,playerY,playerSize,playerSize);
+}
+let gameLoop = setInterval(()=>{
+    Update();
+    //렌더
+    Render();
+
+},1000/60)
+
+function A()
+{
+    this.a = 10;
+}
+let c = new A();
