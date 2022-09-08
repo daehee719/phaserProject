d76bc23c958fbb16bf34a27288a36be6f1fa5de0
강대희 <daeheuigang@gmail.com>
2022-09-08 09:16:00 +0900

    총알 구현
    충돌 체크, 기본 세팅
    이제 플레이어 충돌 체크, 생성, 날아가기 해야됨

--- jsProject/src/Bullet.ts
+++ jsProject/src/Bullet.ts
@@ -3,11 +3,30 @@ export class Bullet {
   y: number;
   size: number;
   speed: number;
-  constructor(x:number,y:number,size:number,speed:number)
-  {
-    this.x= x;
+
+  constructor(x: number, y: number, size: number, speed: number) {
+    this.x = x;
     this.y = y;
     this.size = size;
     this.speed = speed;
   }
+
+  isCollision(
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D
+  ): boolean {
+    if (
+      this.x == 0 ||
+      this.y == 0 ||
+      this.x == canvas.width - this.size ||
+      this.x == canvas.height - this.size
+    ) {
+      ctx.clearRect(this.x, this.y, this.size, this.size);
+      return true;
+    }
+    return false;
+    // else if(this.x ==  ) 플레이어 충돌 체크
+  }
+
+  update(dt: number): void {}
 }
--- jsProject/src/app.ts
+++ jsProject/src/app.ts
@@ -1,15 +1,17 @@
 import { Player } from "./Player.js";
+import { Bullet } from "./Bullet.js";
 
 class App {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
 
   private player: Player;
+  private bullet: Bullet;
   constructor(selector: string) {
     this.canvas = document.querySelector(selector) as HTMLCanvasElement;
-
     this.ctx = this.canvas?.getContext("2d") as CanvasRenderingContext2D;
     this.player = new Player(200, 200, 30, 100);
+    this.bullet = new Bullet(400, 400, 10, 300);
     this.loop();
   }
 
@@ -22,6 +24,7 @@ class App {
   }
   update(dt: number): void {
     this.player.update(dt);
+    this.bullet.update(dt);
   }
   render(): void {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
